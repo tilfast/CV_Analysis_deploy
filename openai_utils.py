45b4354dc80a02784840dcb692146499fa0eaf2b
Gilles <tilfast@yahoo.com>
2025-08-26 08:31:49 +0900

    try to correct API key bug  v1

--- openai_utils.py
+++ openai_utils.py
@@ -23,6 +23,9 @@ try:
 except Exception:
     st = None
 
+print("openai.api_key:", os.getenv("OPENAI_API_KEY"))
+    
+    
 def _get_api_key() -> str:
     key = os.getenv("OPENAI_API_KEY")
     if key:
